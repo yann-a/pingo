44d518920eb93db4d1769cff8d2117fad9a0e02b
Yann Aguettaz <yann.aguettaz@ens-lyon.fr>
2020-04-15 16:43:28 +0200

    print

--- expr.go
+++ expr.go
@@ -1,6 +1,5 @@
 package main
 
-import "fmt"
 
 // Expressions
 type expr interface{
@@ -11,22 +10,14 @@ type expr interface{
 
 type parallel []expr
 func (e parallel) isExpr() { }
-func (e parallel) String() (st string) {
-  for i,v := range e {
-    if i>0 { st = st+" | "}
-    st = st + v.String()
-  }
-  return
-}
+
 
 type send struct { // send a value on a given channel
   channel string
   value terminal
 }
 func (e send) isExpr() { }
-func (e send) String() string {
-  return fmt.Sprintf("^%v %v", e.channel, e.value)
-}
+
 
 type receiveThen struct { // receive a value then execute a process
   channel string
@@ -34,31 +25,24 @@ type receiveThen struct { // receive a value then execute a process
   then expr
 }
 func (e receiveThen) isExpr() { }
-func (e receiveThen) String() string {
-  return fmt.Sprintf("%v(%v).%v", e.channel, e.pattern, e.then)
-}
+
 
 type privatize struct { // used to define private channels
   channel string
   then expr
 }
 func (e privatize) isExpr() { }
-func (e privatize) String() string {
-  return fmt.Sprintf("(%v).%v", e.channel, e.then)
-}
+
 
 type print struct {
   v terminal
   then expr
 }
 func (e print) isExpr() { }
-func (e print) String() string {
-  return fmt.Sprintf("print %v; %v", e.v, e.then)
-}
+
 
 type skip int
 func (e skip) isExpr() { }
-func (e skip) String() (r string) { return }
 
 
 
---
+++ print.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func (e parallel)    String() (st string) {
+  st = "("
+  for i,v := range e {
+    if i>0 { st = st+" | "}
+    st = st + v.String()
+  }
+  st += ")"
+  return
+}
+
+func (e send)        String() string      {
+  return fmt.Sprintf("(^%v %v)", e.channel, e.value)
+}
+
+func (e receiveThen) String() string      {
+  return fmt.Sprintf("(%v(%v).%v)", e.channel, e.pattern, e.then)
+}
+
+func (e privatize)   String() string      {
+  return fmt.Sprintf("((%v).%v)", e.channel, e.then)
+}
+
+func (e print)       String() string      {
+  return fmt.Sprintf("(print %v; %v)", e.v, e.then)
+}
+
+func (e skip) String() string { return "skip"}
