39be41bfd880cdcaaf4d9ba4029e689ac25dd795
Guilhem Niot <guilhem.niot@gmail.com>
2020-04-15 21:50:25 +0200

    Revert fix for go deadlocks

--- environment.go
+++ environment.go
@@ -7,17 +7,8 @@ type value interface {
   isValue()
 }
 
-type channel struct {
-  ch chan value
-  mux *sync.Mutex
-  counter *int // On doit compter le nombre d'envoyeurs / récepteurs en attente sur le canal pour savoir combien on a de goroutine active
-               // et pouvoir déterminer quand on peut arrêter le process (tout le monde en attente)
-}
+type channel chan value
 func (c channel) isValue() { }
-func createChannel() channel {
-  counter := 0
-  return channel{make(chan value), &sync.Mutex{}, &counter}
-}
 
 // type constant is defined in eval.go and is also a terminal
 func (c constant) isValue() { }
@@ -46,7 +37,7 @@ var accessToEnd sync.Mutex // The end of the environment cannot be updated by tw
 
 func (e *env) get_value(x variable) value {
   if (e == nil) {
-    channel := createChannel()
+    channel := make(channel)
     e = &env{x, channel, nil} // On ajoute le nouveau channel dans l'espace global en le mettant à la fin de l'environnement
 
     return channel
@@ -61,7 +52,7 @@ func (e *env) get_value(x variable) value {
     defer accessToEnd.Unlock() // And make sure it's unlocked once we're done
 
     if e.next == nil { // If no concurrent access before getting the lock
-      channel := createChannel()
+      channel := make(channel)
       e.next = &env{x, channel, nil} // On ajoute le nouveau channel dans l'espace global en le mettant à la fin de l'environnement
 
       return channel
--- eval.go
+++ eval.go
@@ -17,42 +17,17 @@ func eval(e expr, envir *env, wg *sync.WaitGroup){
 
 
 		case send:
-			channel := envir.get_value(variable(v.channel)).(channel)
 			val, ok := interpretTerminal(v.value, envir)
 			if !ok {
 				fmt.Println("Error while sending: not a value provided")
 				return
 			}
 
-			channel.mux.Lock() // on a besoin de prendre le lock sur le channel avant de l'appeler pour savoir si on va être bloqué ou non
-			blocking := *channel.counter > 0
-			*channel.counter -= 1
-			if blocking { // Aucun receveur attend, le channel va bloqué une goroutine, on réduit le nb de goroutine qui s'exécute
-				wg.Done()
-			}
-			channel.mux.Unlock()
-
-			channel.ch <- val
-			if blocking {
-				wg.Add(1)
-			}
+			envir.get_value(variable(v.channel)).(channel) <- val
 
 
 		case receiveThen:
-			channel := envir.get_value(variable(v.channel)).(channel)
-
-			channel.mux.Lock()
-			blocking := *channel.counter < 0
-			*channel.counter += 1
-			if blocking { // Aucun envoyeur attend, le channel va bloqué une goroutine, on réduit le nb de goroutine qui s'exécute
-				wg.Done()
-			}
-			channel.mux.Unlock()
-
-			message := <- channel.ch
-			if blocking { // On réaugmente quand on finit l'écoute
-				wg.Add(1)
-			}
+			message := <- envir.get_value(variable(v.channel)).(channel)
 
 			switch pattern := v.pattern.(type) {
 			case variable:
@@ -68,7 +43,7 @@ func eval(e expr, envir *env, wg *sync.WaitGroup){
 
 
 		case privatize:
-			envir2 := envir.set_value(variable(v.channel), createChannel())
+			envir2 := envir.set_value(variable(v.channel), make(channel))
 
 			wg.Add(1)
 			eval(v.then, envir2, wg)
