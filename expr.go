7f92f8bdc58a5df11ac3f2eb6e81dbf102613782
Yann Aguettaz <yann.aguettaz@ens-lyon.fr>
2020-04-15 16:24:44 +0200

    print

--- expr.go
+++ expr.go
@@ -1,42 +1,64 @@
 package main
 
+import "fmt"
+
 // Expressions
 type expr interface{
 	isExpr()
+  String() string
 }
 
 
 type parallel []expr
-func (c parallel) isExpr() { }
+func (e parallel) isExpr() { }
+func (e parallel) String() (st string) {
+  for i,v := range e {
+    if i>0 { st = st+" | "}
+    st = st + v.String()
+  }
+  return
+}
 
 type send struct { // send a value on a given channel
   channel string
   value terminal
 }
-func (c send) isExpr() { }
+func (e send) isExpr() { }
+func (e send) String() string {
+  return fmt.Sprintf("^%v %v", e.channel, e.value)
+}
 
 type receiveThen struct { // receive a value then execute a process
   channel string
   pattern terminal
   then expr
 }
-func (c receiveThen) isExpr() { }
+func (e receiveThen) isExpr() { }
+func (e receiveThen) String() string {
+  return fmt.Sprintf("%v(%v).%v", e.channel, e.pattern, e.then)
+}
 
 type privatize struct { // used to define private channels
   channel string
   then expr
 }
-func (c privatize) isExpr() { }
+func (e privatize) isExpr() { }
+func (e privatize) String() string {
+  return fmt.Sprintf("(%v).%v", e.channel, e.then)
+}
 
 type print struct {
   v terminal
   then expr
 }
-func (c print) isExpr() { }
+func (e print) isExpr() { }
+func (e print) String() string {
+  return fmt.Sprintf("print %v; %v", e.v, e.then)
+}
 
 type skip int
-func (c skip) isExpr() { }
-
+func (e skip) isExpr() { }
+func (e skip) String() (r string) { return }
 
 
 
