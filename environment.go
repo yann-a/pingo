29c71d6f04246ae94f78d7c38b5e929d7cc319fc
Yann Aguettaz <yann.aguettaz@ens-lyon.fr>
2020-04-16 13:59:17 +0200

    GrRrRr

--- environment.go
+++ environment.go
@@ -2,65 +2,69 @@ package main
 
 import "sync"
 
-// Concrete values
+/**** Concrete values ****/
 type value interface {
-  isValue()
+	isValue()
 }
 
-// channel defined in channel.go
+// channels are values (they can be sent through channels)
 type channel chan value
-func (c channel) isValue() { }
+
+func (c channel) isValue() {}
 
 // type constant is defined in eval.go and is also a terminal
-func (c constant) isValue() { }
+func (c constant) isValue() {}
 
 type vpair struct {
-  v1 value
-  v2 value
+	v1 value
+	v2 value
 }
-func (c vpair) isValue() { }
 
+func (c vpair) isValue() {}
 
-// Environment management
+/**** Environment ****/
 type env struct {
-  name variable
-  value value
-  next *env
+	name  variable
+	value value
+	next  *env
 }
 
+/**** Methods ****/
+// We append the value at the front of the environment
 func (e *env) set_value(x variable, v value) *env {
-  return &env{x, v, e}
+	return &env{x, v, e}
 }
 
-
-var accessToEnd sync.Mutex // The end of the environment cannot be updated by two goroutines at the same time
-                           // We prevent that from happening by using a mutex
+// The end of the environment cannot be updated by two goroutines at the same time
+// We prevent that from happening by using a mutex
+var accessToEnd sync.Mutex
 
 func (e *env) get_value(x variable) value {
-  if (e == nil) {
-    channel := make(channel)
-    e = &env{x, channel, nil} // On ajoute le nouveau channel dans l'espace global en le mettant à la fin de l'environnement
+	if e == nil {
+		channel := make(channel)
+		// We add the new channel in the global space by appending it at the end of the environment
+		e = &env{x, channel, nil}
 
-    return channel
-  }
+		return channel
+	}
 
-  if e.name == x {
-    return e.value
-  }
+	if e.name == x {
+		return e.value
+	}
 
-  if e.next == nil { // on a atteint la fin de l'environnement
-    accessToEnd.Lock() // To avoid interferences between processes, we lock the mutex
-    defer accessToEnd.Unlock() // And make sure it's unlocked once we're done
+	// If we reach the end of the environment
+	if e.next == nil {
+		accessToEnd.Lock()         // To avoid interferences between processes, we lock the mutex
+		defer accessToEnd.Unlock() // And make sure it's unlocked once we're done
 
-    if e.next == nil { // If no concurrent access before getting the lock
-      channel := make(channel)
-      e.next = &env{x, channel, nil} // On ajoute le nouveau channel dans l'espace global en le mettant à la fin de l'environnement
+		if e.next == nil { // If no concurrent access before getting the lock
+			channel := make(channel)
+			// We add the new channel in the global space by appending it at the end of the environment
+			e.next = &env{x, channel, nil}
+			return channel
+		}
+	}
 
-      return channel
-    }
-  }
-
-
-  // Otherwise we dive deeper in the environment
-  return e.next.get_value(x)
+	// Otherwise we dive deeper in the environment
+	return e.next.get_value(x)
 }
--- eval.go
+++ eval.go
@@ -5,171 +5,163 @@ import (
 	"sync"
 )
 
-func eval(e expr, envir *env, wg *sync.WaitGroup){
-	defer wg.Done()
+func eval(e expr, envir *env, wg *sync.WaitGroup) {
+	defer wg.Done() // We make sure we mark the process as finished at the end
 
-	switch v := e.(type) {
-		case parallel:
-			for _, task := range(v) {
-				wg.Add(1)
-				go eval(task, envir, wg)
-			}
+	switch v := e.(type) { // We do some kind of pattern matching on the expression
+	case parallel:
+		for _, task := range v {
+			wg.Add(1)
+			// We run each task in parallel in separate goroutines
+			go eval(task, envir, wg)
+		}
 
+	case send:
+		val, ok := interpretTerminal(v.value, envir)
+		if !ok {
+			fmt.Println("Error while sending: not a value provided\n")
+			return
+		}
 
-		case send:
-			val, ok := interpretTerminal(v.value, envir)
-			if !ok {
-				fmt.Println("Error while sending: not a value provided\n")
-				return
-			}
+		channel := envir.get_value(variable(v.channel)).(channel)
+		channel <- val
 
-			channel := envir.get_value(variable(v.channel)).(channel)
-			channel <- val
+	case receiveThen:
+		channel := envir.get_value(variable(v.channel)).(channel)
+		message := <-channel
 
+		switch pattern := v.pattern.(type) {
+		case variable:
+			envir = envir.set_value(pattern, message)
+		case pair:
+			pair := message.(vpair)
+			envir = envir.set_value(pattern.v1.(variable), pair.v1)
+			envir = envir.set_value(pattern.v2.(variable), pair.v2)
+		}
 
-		case receiveThen:
-			channel := envir.get_value(variable(v.channel)).(channel)
-			message := <- channel
+		eval(v.then, envir, wg)
+		wg.Add(1)
 
-			switch pattern := v.pattern.(type) {
-			case variable:
-				envir = envir.set_value(pattern, message)
-			case pair:
-				pair := message.(vpair)
-				envir = envir.set_value(pattern.v1.(variable), pair.v1)
-				envir = envir.set_value(pattern.v2.(variable), pair.v2)
-			}
+	case privatize:
+		envir2 := envir.set_value(variable(v.channel), make(channel))
 
+		eval(v.then, envir2, wg)
+		wg.Add(1)
+
+	case print:
+		ret, ok := interpretTerminal(v.v, envir)
+		if !ok {
+			fmt.Println("Error while printing: not a value provided\n")
+			return
+		}
+
+		integer := ret.(constant)
+
+		fmt.Printf("%d\n", int(integer))
+
+		eval(v.then, envir, wg)
+		wg.Add(1)
+
+	case skip:
+		// nothing to do here
+
+	// case choose:
+	// 	var c1, c2 string
+	// 	var p, p1, p2 terminal
+	// 	var t, t1, t2 expr
+	// 	var message value
+	//
+	// 	switch ve := v.e.(type) {
+	// 		case receiveThen:
+	// 			c1 = ve.channel
+	// 			p1 = ve.pattern
+	// 			t1 = ve.then
+	// 		default:
+	// 			fmt.Println("Processes to choose from can only start by reading : not the case of %v\n", ve)
+	// 			return
+	// 	}
+	// 	switch vf := v.f.(type) {
+	// 		case receiveThen:
+	// 			c2 = vf.channel
+	// 			p2 = vf.pattern
+	// 			t2 = vf.then
+	// 		default:
+	// 			fmt.Println("Processes to choose from can only start by reading : not the case of %v\n", vf)
+	// 			return
+	// 	}
+	//
+	// 	wg.Done() // on réduit de 1 le compte des eval en cours d'exécution tant qu'on attend un message
+	// 	select {
+	// 		case message = <-envir.get_value(variable(c1)).(channel):
+	// 			p = p1
+	// 			t = t1
+	// 		case message = <-envir.get_value(variable(c2)).(channel):
+	// 			p = p2
+	// 			t = t2
+	// 	}
+	// 	wg.Add(1) // On réaugmente quand on finit l'écoute
+	//
+	// 	switch pattern := p.(type) {
+	// 		case variable:
+	// 			envir = envir.set_value(pattern, message)
+	// 		case pair:
+	// 			pair := message.(vpair)
+	// 			envir = envir.set_value(pattern.v1.(variable), pair.v1)
+	// 			envir = envir.set_value(pattern.v2.(variable), pair.v2)
+	// 	}
+	//
+	// 	wg.Add(1)
+	// 	eval(t, envir, wg)
+
+	case conditional:
+		val_l, ok_l := interpretTerminal(v.e, envir)
+		val_r, ok_r := interpretTerminal(v.f, envir)
+		if !ok_l || !ok_r {
+			fmt.Printf("Error : can't compare non-values expressions (%v and %v)\n", v.e, v.f)
+			return
+		}
+
+		if v.eq == (val_l == val_r) {
 			eval(v.then, envir, wg)
 			wg.Add(1)
+		}
 
+	case repl:
+		channel := envir.get_value(variable(v.channel)).(channel)
+		message := <-channel
 
-		case privatize:
-			envir2 := envir.set_value(variable(v.channel), make(channel))
+		switch pattern := v.pattern.(type) {
+		case variable:
+			envir = envir.set_value(pattern, message)
+		case pair:
+			pair := message.(vpair)
+			envir = envir.set_value(pattern.v1.(variable), pair.v1)
+			envir = envir.set_value(pattern.v2.(variable), pair.v2)
+		}
 
-			eval(v.then, envir2, wg)
-			wg.Add(1)
+		wg.Add(1)
+		go eval(v.then, envir, wg)
+		eval(v, envir, wg)
+		wg.Add(1)
 
-
-		case print:
-			ret, ok := interpretTerminal(v.v, envir)
-			if !ok {
-				fmt.Println("Error while printing: not a value provided\n")
-				return
-			}
-
-			integer := ret.(constant)
-
-			fmt.Printf("%d\n", int(integer))
-
-			eval(v.then, envir, wg)
-			wg.Add(1)
-
-
-		case skip:
-			// nothing to do here
-
-
-		// case choose:
-		// 	var c1, c2 string
-		// 	var p, p1, p2 terminal
-		// 	var t, t1, t2 expr
-		// 	var message value
-		//
-		// 	switch ve := v.e.(type) {
-		// 		case receiveThen:
-		// 			c1 = ve.channel
-		// 			p1 = ve.pattern
-		// 			t1 = ve.then
-		// 		default:
-		// 			fmt.Println("Processes to choose from can only start by reading : not the case of %v\n", ve)
-		// 			return
-		// 	}
-		// 	switch vf := v.f.(type) {
-		// 		case receiveThen:
-		// 			c2 = vf.channel
-		// 			p2 = vf.pattern
-		// 			t2 = vf.then
-		// 		default:
-		// 			fmt.Println("Processes to choose from can only start by reading : not the case of %v\n", vf)
-		// 			return
-		// 	}
-		//
-		// 	wg.Done() // on réduit de 1 le compte des eval en cours d'exécution tant qu'on attend un message
-		// 	select {
-		// 		case message = <-envir.get_value(variable(c1)).(channel):
-		// 			p = p1
-		// 			t = t1
-		// 		case message = <-envir.get_value(variable(c2)).(channel):
-		// 			p = p2
-		// 			t = t2
-		// 	}
-		// 	wg.Add(1) // On réaugmente quand on finit l'écoute
-		//
-		// 	switch pattern := p.(type) {
-		// 		case variable:
-		// 			envir = envir.set_value(pattern, message)
-		// 		case pair:
-		// 			pair := message.(vpair)
-		// 			envir = envir.set_value(pattern.v1.(variable), pair.v1)
-		// 			envir = envir.set_value(pattern.v2.(variable), pair.v2)
-		// 	}
-		//
-		// 	wg.Add(1)
-		// 	eval(t, envir, wg)
-
-
-		case conditional:
-			val_l, ok_l := interpretTerminal(v.e, envir)
-			val_r, ok_r := interpretTerminal(v.f, envir)
-			if !ok_l || !ok_r {
-				fmt.Printf("Error : can't compare non-values expressions (%v and %v)\n", v.e, v.f)
-				return
-			}
-
-			if v.eq == (val_l == val_r) {
-				eval(v.then, envir, wg)
-				wg.Add(1)
-			}
-
-
-		case repl:
-			channel := envir.get_value(variable(v.channel)).(channel)
-			message := <- channel // on attend sa réponse sur le canal privé
-
-			switch pattern := v.pattern.(type) {
-			case variable:
-				envir = envir.set_value(pattern, message)
-			case pair:
-				pair := message.(vpair)
-				envir = envir.set_value(pattern.v1.(variable), pair.v1)
-				envir = envir.set_value(pattern.v2.(variable), pair.v2)
-			}
-
-			wg.Add(1)
-			go eval(v.then, envir, wg)
-			eval(v, envir, wg)
-			wg.Add(1)
-
-		default:
-			fmt.Printf("unrecognised type %T (%v)\n", v, v)
+	default:
+		fmt.Printf("unrecognised type %T (%v)\n", v, v)
 	}
 }
-
 
 // Transform a terminal expression into a value
 func interpretTerminal(val terminal, envir *env) (value, bool) {
 	switch v := val.(type) {
-		case constant:
-			return v, true
-		case variable:
-			return envir.get_value(v), true
-		case pair:
-			v1, ok1 := interpretTerminal(v.v1, envir)
-			v2, ok2 := interpretTerminal(v.v2, envir)
+	case constant:
+		return v, true
+	case variable:
+		return envir.get_value(v), true
+	case pair:
+		v1, ok1 := interpretTerminal(v.v1, envir)
+		v2, ok2 := interpretTerminal(v.v2, envir)
 
-			return vpair{v1, v2}, ok1 && ok2
-		default:
-			return constant(0), false
+		return vpair{v1, v2}, ok1 && ok2
+	default:
+		return constant(0), false
 	}
 }
