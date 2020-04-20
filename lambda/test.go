c46c863cf5ae4f7ac598fac0186208246e3035d8
Yann Aguettaz <yann.aguettaz@ens-lyon.fr>
2020-04-20 22:35:11 +0200

    Make guilhem happy

--- lambda/test.go
+++ lambda/test.go
@@ -4,10 +4,22 @@ import (
 	"bufio"
 	"os"
 	"fmt"
+	"flag"
 )
 
 func Test() {
-	in := bufio.NewReader(os.Stdin)
+	nonFlagsArgs := flag.Args()
+	buffer := os.Stdin
+	// If a file is provided we try reading from it
+	if len(nonFlagsArgs) > 0 {
+		file, err := os.Open(nonFlagsArgs[0])
+		if err != nil {
+			fmt.Printf("Couldn't read from %s (%s). Reading from stdin\n", nonFlagsArgs[0], err)
+		} else {
+			buffer = file
+		}
+	}
+	in := bufio.NewReader(buffer)
 
 	lex := &lambdaLex{reader: in}
 	lambdaParse(lex)
