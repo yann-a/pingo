5d9cc7a81ab9b842ce6e0b66440507ba457abf4a
Guilhem Niot <guilhem.niot@gmail.com>
2020-04-22 16:15:23 +0200

    Translate lambda arithmetic as sync code

--- src/translate/translate.go
+++ src/translate/translate.go
@@ -28,18 +28,22 @@ func Translate(lexpr lambda.Lambda, channel string) pi.Expr {
 		// Une fonction lambda est transformée en un canal qui reçoit des paires (argument, canal de retour)
 		return pi.Privatize{"y", pi.Parallel{pi.Send{channel, pi.Variable("y")}, pi.Repl{"y", pi.Pair{pi.Variable(v.Arg), pi.Variable("q")}, Translate(v.Exp, "q")}}}
 	case lambda.Lapp:
-		return pi.Privatize{channel1, pi.Parallel{Translate(v.Exp, channel1), pi.ReceiveThen{channel1, pi.Variable("v"), pi.Privatize{channel2, pi.Parallel{Translate(v.Fun, channel2), pi.ReceiveThen{channel2, pi.Variable("f"), pi.Send{"f", pi.Pair{pi.Variable("v"), pi.Variable(channel)}}}}}}}}
+		return translateArith(v.Fun, v.Exp, pi.Send{"lresult", pi.Pair{pi.Variable("rresult"), pi.Variable(channel)}}, channel1, channel2)
 	case lambda.Add:
-		return pi.Privatize{channel1, pi.Privatize{channel2, pi.Parallel{Translate(v.L, channel1), Translate(v.R, channel2), pi.ReceiveThen{channel1, pi.Variable("lresult"), pi.ReceiveThen{channel2, pi.Variable("rresult"), pi.Send{channel, pi.Add{pi.Variable("lresult"), pi.Variable("rresult")}}}}}}}
+		return translateArith(v.L, v.R, pi.Send{channel, pi.Add{pi.Variable("lresult"), pi.Variable("rresult")}}, channel1, channel2)
 	case lambda.Sub:
-		return pi.Privatize{channel1, pi.Privatize{channel2, pi.Parallel{Translate(v.L, channel1), Translate(v.R, channel2), pi.ReceiveThen{channel1, pi.Variable("lresult"), pi.ReceiveThen{channel2, pi.Variable("rresult"), pi.Send{channel, pi.Sub{pi.Variable("lresult"), pi.Variable("rresult")}}}}}}}
+		return translateArith(v.L, v.R, pi.Send{channel, pi.Sub{pi.Variable("lresult"), pi.Variable("rresult")}}, channel1, channel2)
 	case lambda.Mult:
-		return pi.Privatize{channel1, pi.Privatize{channel2, pi.Parallel{Translate(v.L, channel1), Translate(v.R, channel2), pi.ReceiveThen{channel1, pi.Variable("lresult"), pi.ReceiveThen{channel2, pi.Variable("rresult"), pi.Send{channel, pi.Mul{pi.Variable("lresult"), pi.Variable("rresult")}}}}}}}
+		return translateArith(v.L, v.R, pi.Send{channel, pi.Mul{pi.Variable("lresult"), pi.Variable("rresult")}}, channel1, channel2)
 	case lambda.Div:
-		return pi.Privatize{channel1, pi.Privatize{channel2, pi.Parallel{Translate(v.L, channel1), Translate(v.R, channel2), pi.ReceiveThen{channel1, pi.Variable("lresult"), pi.ReceiveThen{channel2, pi.Variable("rresult"), pi.Send{channel, pi.Div{pi.Variable("lresult"), pi.Variable("rresult")}}}}}}}
+		return translateArith(v.L, v.R, pi.Send{channel, pi.Div{pi.Variable("lresult"), pi.Variable("rresult")}}, channel1, channel2)
 	case lambda.Print:
 		return pi.Privatize{channel1, pi.Parallel{Translate(v.L, channel1), pi.ReceiveThen{channel1, pi.Variable("result"), pi.Print{pi.Variable("result"), pi.Send{channel, pi.Variable("result")}}}}}
 	default:
 		panic("not supposed to happen")
 	}
+}
+
+func translateArith(L lambda.Lambda, R lambda.Lambda, sendExpr pi.Expr, channel1 string, channel2 string) pi.Expr {
+	return pi.Privatize{channel2, pi.Parallel{Translate(R, channel2), pi.ReceiveThen{channel2, pi.Variable("rresult"), pi.Privatize{channel1, pi.Parallel{Translate(R, channel1), pi.ReceiveThen{channel1, pi.Variable("lresult"), sendExpr}}}}}}
 }
