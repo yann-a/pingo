db089d2b0937a702708167e165de6a42e603928a
Guilhem Niot <guilhem.niot@gmail.com>
2020-04-22 12:00:21 +0200

    Fix lambda translation

--- src/translate/translate.go
+++ src/translate/translate.go
@@ -6,23 +6,37 @@ import (
 )
 
 func Translate(lexpr lambda.Lambda, channel string) pi.Expr {
+	// on détermine des noms frais pour les translate récursifs
+	var channel1, channel2 string
+	if channel == "q" {
+		channel1 = "r"
+		channel2 = "s"
+	} else if channel == "r" {
+		channel1 = "q"
+		channel2 = "s"
+	} else {
+		channel1 = "q"
+		channel2 = "r"
+	}
+
 	switch v := lexpr.(type) {
 	case lambda.Lconst:
 		return pi.Send{channel, pi.Constant(v)}
 	case lambda.Lvar:
 		return pi.Send{channel, pi.Variable(v)}
 	case lambda.Lfun:
-		return pi.Privatize{"y", pi.Parallel{pi.Send{channel, pi.Variable("y")}, pi.ReceiveThen{"y", pi.Pair{pi.Variable(v.Arg), pi.Variable("q")}, Translate(v.Exp, "q")}}}
+		// Une fonction lambda est transformée en un canal qui reçoit des paires (argument, canal de retour)
+		return pi.Privatize{"y", pi.Parallel{pi.Send{channel, pi.Variable("y")}, pi.Repl{"y", pi.Pair{pi.Variable(v.Arg), pi.Variable("q")}, Translate(v.Exp, "q")}}}
 	case lambda.Lapp:
-		return pi.Privatize{"q", pi.Parallel{Translate(v.Exp, "q"), pi.ReceiveThen{"q", pi.Variable("v"), pi.Privatize{"r", pi.Parallel{Translate(v.Fun, "r"), pi.ReceiveThen{"r", pi.Variable("f"), pi.Send{"f", pi.Pair{pi.Variable("v"), channel}}}}}}}}
+		return pi.Privatize{channel1, pi.Parallel{Translate(v.Exp, channel1), pi.ReceiveThen{channel1, pi.Variable("v"), pi.Privatize{channel2, pi.Parallel{Translate(v.Fun, channel2), pi.ReceiveThen{channel2, pi.Variable("f"), pi.Send{"f", pi.Pair{pi.Variable("v"), pi.Variable(channel)}}}}}}}}
 	case lambda.Add:
-		return pi.Privatize{"l", pi.Privatize{"r", pi.Parallel{Translate(v.L, "l"), Translate(v.R, "r"), pi.ReceiveThen{"l", pi.Variable("lresult"), pi.ReceiveThen{"r", pi.Variable("rresult"), pi.Send{channel, pi.Add{pi.Variable("lresult"), pi.Variable("rresult")}}}}}}}
+		return pi.Privatize{channel1, pi.Privatize{channel2, pi.Parallel{Translate(v.L, channel1), Translate(v.R, channel2), pi.ReceiveThen{channel1, pi.Variable("lresult"), pi.ReceiveThen{channel2, pi.Variable("rresult"), pi.Send{channel, pi.Add{pi.Variable("lresult"), pi.Variable("rresult")}}}}}}}
 	case lambda.Sub:
-		return pi.Privatize{"l", pi.Privatize{"r", pi.Parallel{Translate(v.L, "l"), Translate(v.R, "r"), pi.ReceiveThen{"l", pi.Variable("lresult"), pi.ReceiveThen{"r", pi.Variable("rresult"), pi.Send{channel, pi.Sub{pi.Variable("lresult"), pi.Variable("rresult")}}}}}}}
+		return pi.Privatize{channel1, pi.Privatize{channel2, pi.Parallel{Translate(v.L, channel1), Translate(v.R, channel2), pi.ReceiveThen{channel1, pi.Variable("lresult"), pi.ReceiveThen{channel2, pi.Variable("rresult"), pi.Send{channel, pi.Sub{pi.Variable("lresult"), pi.Variable("rresult")}}}}}}}
 	case lambda.Mult:
-		return pi.Privatize{"l", pi.Privatize{"r", pi.Parallel{Translate(v.L, "l"), Translate(v.R, "r"), pi.ReceiveThen{"l", pi.Variable("lresult"), pi.ReceiveThen{"r", pi.Variable("rresult"), pi.Send{channel, pi.Mul{pi.Variable("lresult"), pi.Variable("rresult")}}}}}}}
+		return pi.Privatize{channel1, pi.Privatize{channel2, pi.Parallel{Translate(v.L, channel1), Translate(v.R, channel2), pi.ReceiveThen{channel1, pi.Variable("lresult"), pi.ReceiveThen{channel2, pi.Variable("rresult"), pi.Send{channel, pi.Mul{pi.Variable("lresult"), pi.Variable("rresult")}}}}}}}
 	case lambda.Div:
-		return pi.Privatize{"l", pi.Privatize{"r", pi.Parallel{Translate(v.L, "l"), Translate(v.R, "r"), pi.ReceiveThen{"l", pi.Variable("lresult"), pi.ReceiveThen{"r", pi.Variable("rresult"), pi.Send{channel, pi.Div{pi.Variable("lresult"), pi.Variable("rresult")}}}}}}}
+		return pi.Privatize{channel1, pi.Privatize{channel2, pi.Parallel{Translate(v.L, channel1), Translate(v.R, channel2), pi.ReceiveThen{channel1, pi.Variable("lresult"), pi.ReceiveThen{channel2, pi.Variable("rresult"), pi.Send{channel, pi.Div{pi.Variable("lresult"), pi.Variable("rresult")}}}}}}}
 	default:
 		panic("not supposed to happen")
 	}
