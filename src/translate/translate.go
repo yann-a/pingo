a6ceeb6043b6bf949d3a8810e56da0f78e58192b
Yann Aguettaz <yann.aguettaz@ens-lyon.fr>
2020-04-22 16:33:08 +0200

    Easier to read translation

--- src/translate/translate.go
+++ src/translate/translate.go
@@ -41,22 +41,100 @@ func Translate(lexpr lambda.Lambda, channel string) pi.Expr {
 			},
 		}
 	case lambda.Lapp:
-		return translateArith(v.Fun, v.Exp, pi.Send{"lresult", pi.Pair{pi.Variable("rresult"), pi.Variable(channel)}}, channel1, channel2)
+		return translateArith(
+			v.Fun,
+			v.Exp,
+			pi.Send{
+				"lresult",
+				pi.Pair{pi.Variable("rresult"), pi.Variable(channel)},
+			},
+			channel1,
+			channel2,
+		)
 	case lambda.Add:
-		return translateArith(v.L, v.R, pi.Send{channel, pi.Add{pi.Variable("lresult"), pi.Variable("rresult")}}, channel1, channel2)
+		return translateArith(
+			v.L,
+			v.R,
+			pi.Send{
+				channel,
+				pi.Add{pi.Variable("lresult"), pi.Variable("rresult")},
+			},
+			channel1,
+			channel2,
+		)
 	case lambda.Sub:
-		return translateArith(v.L, v.R, pi.Send{channel, pi.Sub{pi.Variable("lresult"), pi.Variable("rresult")}}, channel1, channel2)
+		return translateArith(
+			v.L,
+			v.R,
+			pi.Send{
+				channel,
+				pi.Sub{pi.Variable("lresult"), pi.Variable("rresult")},
+			},
+			channel1,
+			channel2,
+		)
 	case lambda.Mult:
-		return translateArith(v.L, v.R, pi.Send{channel, pi.Mul{pi.Variable("lresult"), pi.Variable("rresult")}}, channel1, channel2)
+		return translateArith(
+			v.L,
+			v.R,
+			pi.Send{
+				channel,
+				pi.Mul{pi.Variable("lresult"), pi.Variable("rresult")},
+			},
+			channel1,
+			channel2,
+		)
 	case lambda.Div:
-		return translateArith(v.L, v.R, pi.Send{channel, pi.Div{pi.Variable("lresult"), pi.Variable("rresult")}}, channel1, channel2)
+		return translateArith(
+			v.L,
+			v.R,
+			pi.Send{
+				channel,
+				pi.Div{pi.Variable("lresult"), pi.Variable("rresult")},
+			},
+			channel1,
+			channel2,
+		)
 	case lambda.Print:
-		return pi.Privatize{channel1, pi.Parallel{Translate(v.L, channel1), pi.ReceiveThen{channel1, pi.Variable("result"), pi.Print{pi.Variable("result"), pi.Send{channel, pi.Variable("result")}}}}}
+		return pi.Privatize{
+			channel1,
+			pi.Parallel{
+				Translate(v.L, channel1),
+				pi.ReceiveThen{
+					channel1,
+					pi.Variable("result"),
+					pi.Print{
+						pi.Variable("result"),
+						pi.Send{channel, pi.Variable("result")},
+					},
+				},
+			},
+		}
 	default:
 		panic("not supposed to happen")
 	}
 }
 
-func translateArith(L lambda.Lambda, R lambda.Lambda, sendExpr pi.Expr, channel1 string, channel2 string) pi.Expr {
-	return pi.Privatize{channel2, pi.Parallel{Translate(R, channel2), pi.ReceiveThen{channel2, pi.Variable("rresult"), pi.Privatize{channel1, pi.Parallel{Translate(R, channel1), pi.ReceiveThen{channel1, pi.Variable("lresult"), sendExpr}}}}}}
+func translateArith(L, R lambda.Lambda, sendExpr pi.Expr, channel1, channel2 string) pi.Expr {
+	return pi.Privatize{
+		channel2,
+		pi.Parallel{
+			Translate(R, channel2),
+			pi.ReceiveThen{
+				channel2,
+				pi.Variable("rresult"),
+				pi.Privatize{
+					channel1,
+					pi.Parallel{
+						Translate(R, channel1),
+						pi.ReceiveThen{
+							channel1,
+							pi.Variable("lresult"),
+							sendExpr,
+						},
+					},
+				},
+			},
+		},
+	}
 }
