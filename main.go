a972fa04fd2ef9750fdca8d49892712f693165e7
Yann Aguettaz <yann.aguettaz@ens-lyon.fr>
2020-05-06 11:44:11 +0200

    remove useless condition

--- main.go
+++ main.go
@@ -31,10 +31,6 @@ func main() {
 	in := bufio.NewReader(buffer)
 	ret := Parse(in, *translateInput, *showSrc)
 
-	if ret == nil {
-		return
-	}
-
 	// We print the code if asked
 	if *outCode {
 		fmt.Println(ret)
